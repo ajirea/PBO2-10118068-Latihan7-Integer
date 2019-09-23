f76d5d4875bde0823cc558be30a22ac4e61d8720
Satria Aji Putra <satriamaxt@gmail.com>
2019-09-23 19:09:47 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan7/integer/PBO210118068Latihan7Integer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan7.integer;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Pada latihan ini yaitu mempelajari penggunaan tipe data
+ * primitif bilangan bulat
+ */
+public class PBO210118068Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 3276;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
